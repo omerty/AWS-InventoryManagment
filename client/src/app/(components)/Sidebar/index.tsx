1fb6eee9264950e3406b141c21d1540736bf27b1
Omer Mohiuddin <mdomermohi@gmail.com>
2024-10-23 14:07:14 -0400

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -82,7 +82,7 @@ const Sidebar = () => {
         }`}
       >
         <Image
-          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
+          src="https://a3-inventorymanagment.s3.us-east-1.amazonaws.com/logo.png"
           alt="edstock-logo"
           width={27}
           height={27}
